51e5993033749aa549b0cc9a40b8aa06e3023704
名字thy“git config --globalgit config --global user.name 名字thy“ <hawktony@126.com>
2019-03-16 23:42:36 +0800

    edit main

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Today is 2019-3-16");
 		System.out.println("Hello  World!");
 	}
 
